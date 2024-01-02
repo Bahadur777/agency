37510404430af9350598d0f45a30b63a86125fd3
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-02 23:35:09 +0600

    hero section start

---
+++ Component/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-container">
+          <div className="hero-conten">
+            <div className="hero-title">
+              <h1 className="title-text">efficient thinking with us !</h1>
+            </div>
+            <div className="hero-middle">
+              <div className="middle-left">
+                  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import TopBar from "@/Component/TopBar/TopBar"
 import Header from "@/Component/Header/Header"
+import Hero from "@/Component/Hero/Hero"
 
 const page = () => {
   return (
     <div>
      <TopBar/>
      <Header/>
+     <Hero/>
     </div>
   )
 }
