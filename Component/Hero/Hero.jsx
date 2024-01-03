b9926b3c65dda626d4f3146f9a914be953be6eea
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-03 10:19:53 +0600

    Support section end

--- Component/Hero/Hero.jsx
+++ Component/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../Button/Button";
 import "./hero.css";
 import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { FcCallback } from "react-icons/fc";
+import { FaSellcast } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -27,6 +29,30 @@ const Hero = () => {
             </div>
             <div className="button-section">
               <Button buttonText="Contact us"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="support-section">
+        <div className="support-container">
+          <div className="support-left">
+            <div className="support-left-icons">
+            <FcCallback />
+            </div>
+            <div className="support-left-text">
+              <div className="s-title">
+                <h1>Support Inquires : </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+            </div>
+          </div>
+          <div className="support-right">
+            <div className="support-right-icons">
+            <FaSellcast />
+            </div>
+            <div className="support-right-content">
+              <h1>Become a Seller: </h1>
+              <p>Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
         </div>
