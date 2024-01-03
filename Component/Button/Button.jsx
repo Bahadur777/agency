97281f8d94f83759efac60099cb06919e299b0d4
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-03 07:50:17 +0600

    hero section complate with a button design

---
+++ Component/Button/Button.jsx
@@ -0,0 +1,11 @@
+import './button.css'
+
+const Button = ({buttonText}) => {
+  return (
+    <>
+     <button className='primary-btn'>{buttonText}</button>
+    </>
+  ) 
+}
+
+export default Button
--- Component/Hero/Hero.jsx
+++ Component/Hero/Hero.jsx
@@ -1,4 +1,6 @@
+import Button from "../Button/Button";
 import "./hero.css";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
 
 const Hero = () => {
   return (
@@ -11,8 +13,20 @@ const Hero = () => {
             </div>
             <div className="hero-middle">
               <div className="middle-left">
-                  
+              <AiOutlineSafetyCertificate />
+              <span>Microsoft gold partner</span>
               </div>
+              <div className="middle-right">
+              <AiOutlineSafetyCertificate />
+              <span>ISO 34243 certified</span>
+              </div>
+            </div>
+            <div className="hr"></div>
+            <div className="hero-text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates aut eius vitae odit iste quisquam consequuntur incidunt quas distinctio recusandae officiis beatae, laboriosam error ut quis? Inventore dignissimos repellendus voluptates.</p>
+            </div>
+            <div className="button-section">
+              <Button buttonText="Contact us"/>
             </div>
           </div>
         </div>
