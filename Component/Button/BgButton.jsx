a2a6ca3f5b492732d46b2d6e04efaad4c2a77b66
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-04 08:24:34 +0600

    service section done and new button design complate

---
+++ Component/Button/BgButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BgButton = ({btnText}) => {
+  return (
+    <button className='bg-btn'>{btnText}</button>
+  )
+}
+
+export default BgButton
--- Component/Service/Service.jsx
+++ Component/Service/Service.jsx
@@ -1,4 +1,7 @@
 import './service.css'
+import BgButton from '../Button/BgButton'
+import Image from 'next/image'
+import Poster from '../../public/people.jpg'
 
 const Service = () => {
   return (
@@ -6,11 +9,23 @@ const Service = () => {
         <div className="service-section">
             <div className="service-container">
                 <div className="service-left">
-                    <div className="service-title">
-                        <h1 className="ser-title-text">
-                            Get the best service for you business from the person you don't even know !
+                    <div className="service-text">
+                        <h1 className="service-title-text">
+                            Get the <span>best service</span> for you <span>business</span> from the <span>person</span> you don't even know !
                         </h1>
+                       
                     </div>
+                    <div className="service-pera">
+                    <p className="service-pera-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, laboriosam? Voluptatibus iste, natus quae porro suscipit quia sequi deserunt maiores!
+                        </p>
+                    </div>
+                    <div className="service-button-section">
+                        <BgButton btnText='Contact Us'/>
+                    </div>
+                </div>
+                <div className="service-right">
+                 <Image src={Poster} className='poster'></Image>
                 </div>
             </div>
         </div>
