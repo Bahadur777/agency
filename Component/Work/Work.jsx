0255834701f15d6b4af1f371b57b1a8a06bea8a6
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-01-06 19:16:25 +0600

    Work component add and title part complate

---
+++ Component/Work/Work.jsx
@@ -0,0 +1,27 @@
+import './work.css'
+
+const Work = () => {
+  return (
+    <div className='work-section'>
+        <div className="work-container">
+            <div className="work-top">
+                <div className="work-s-text">
+                    <span>How we work</span>
+                </div>
+                <div className="work-title">
+                    <h1 className="workTitle-text">
+                       We Have some <span>easy steps</span> to process
+                    </h1>
+                </div>
+                <div className="work-sub-title">
+                    <p className="workSub-text">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nobis?    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import TopBar from "@/Component/TopBar/TopBar"
 import Header from "@/Component/Header/Header"
 import Hero from "@/Component/Hero/Hero"
 import Service from "@/Component/Service/Service"
+import Work from "@/Component/Work/Work"
 
 const page = () => {
   return (
@@ -10,6 +11,7 @@ const page = () => {
      <Header/>
      <Hero/>
      <Service/>
+     <Work/>
     </div>
   )
 }
